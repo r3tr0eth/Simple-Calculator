9e18332fd993ed95fcd7f75dedc6726e76ef7805
r3tr0eth <jimenezgaizka13@protonmail.com>
2023-04-23 23:14:33 +0200

    FEAT - functions

---
+++ calculator.py
@@ -0,0 +1,17 @@
+# Simple Calculator
+
+# Define functions for basic math operations
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Cannot divide by zero"
+    else:
+        return x / y
