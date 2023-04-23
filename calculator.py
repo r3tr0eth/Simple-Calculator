973bda216fc258c8983c123c76458e3a36780a9f
r3tr0eth <jimenezgaizka13@protonmail.com>
2023-04-23 23:24:13 +0200

    FEAT - Main program loop

--- calculator.py
+++ calculator.py
@@ -29,5 +29,27 @@ def get_choice():
     print("2. Subtract")
     print("3. Multiply")
     print("4. Divide")
-    choice = input("Enter choice (1/2/3/4): ")
+    choice = input("Enter choice (1/2/3/4/5): ")
     return choice
+
+# Main program loop
+while True:
+    choice = get_choice()
+
+    if choice in ('1', '2', '3', '4'):
+        num1, num2 = get_input()
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+
+    else:
+        print("Invalid Input")
